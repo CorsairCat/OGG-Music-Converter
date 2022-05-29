fc41bc6d7e23868a334297e0fa5469f66be9303c
CorsairCat <1274056321@qq.com>
2022-05-30 00:36:51 +0100

    unable to process utf type meta data

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,26 @@
 
 int main()
 {
-    std::string filename = "../Sample/sample.mp3";
+    std::string filename = "../Sample/朴树-平凡之路.mp3";
     DCSToolBox::Mp3ID1Tag container;
     int status = DCSToolBox::collectMP3MetaDataResult(filename.c_str(), &container);
+    DCSToolBox::Tag4Display metaData;
+    if (status == _LOAD_METADATE_FAIL_)
+    {
+        // use the direct file name
+        metaData.title = filename;
+        metaData.artist = "unknow";
+        metaData.album = "unknow";
+        metaData.year = 2000;
+    }
+    else
+    {
+        metaData.title.assign(container.title, 30);
+        metaData.artist.assign(container.artist, 30);
+        metaData.album.assign(container.album, 30);
+        metaData.year = ((int)container.year[0]-48) * 1000 + ((int)container.year[1]-48) * 100 + ((int)container.year[2]-48) * 10 + ((int)container.year[3]-48);
+    }
+
+    DCSToolBox::printCollectedMetaData(metaData);
+    return 1;
 }
--- src/meta_data_collect.h
+++ src/meta_data_collect.h
@@ -12,7 +12,15 @@
 namespace DCSToolBox
 {
 
-typedef struct{
+struct Tag4Display
+{
+    std::string title;
+    std::string artist;
+    std::string album;
+    int year;
+};
+
+struct Mp3ID1Tag{
    char tag[3];
    char title[30];
    char artist[30];
@@ -20,7 +28,7 @@ typedef struct{
    char year[4];
    char comment[30];
    unsigned char genre;
-} Mp3ID1Tag;
+};
 
 int collectMP3MetaDataResult(const char* file_name, Mp3ID1Tag* tag_buffer)
 {
@@ -29,48 +37,42 @@ int collectMP3MetaDataResult(const char* file_name, Mp3ID1Tag* tag_buffer)
     FILE *fp = fopen(file_name, "rb");
 	if (!fp)
 	{
-		std::cout << "File open failed" << "\n\r";
+		std::cout << "Error: File open failed" << "\n\r";
 		return _LOAD_METADATE_FAIL_;
 	}
 
 	// Seek to 128 bytes before the end of the file
 	if (fseek(fp, -1 * sizeof(Mp3ID1Tag), SEEK_END) == -1)
 	{
-        std::cout << "Can't find END" << "\n\r";
+        std::cout << "Error: Can't find END" << "\n\r";
 		return _LOAD_METADATE_FAIL_;
 	}
 
 	// Read the tag
 	if (fread(tag_buffer, sizeof(Mp3ID1Tag), 1, fp) != 1)
 	{
-		std::cout << "Failed reading tag" << "\n\r";
+		std::cout << "Error: Failed reading tag" << "\n\r";
 		return _LOAD_METADATE_FAIL_;
 	}
 
 	if (memcmp(tag_buffer->tag, "TAG", 3) == 0)
 	{
-		std::cout << "Title: " << tag_buffer->title << "\n\r";
-        std::cout << "Artist: " << tag_buffer->artist << "\n\r";
-        std::cout << "Album: " << tag_buffer->album << "\n\r";
-        std::cout << "Year: " << tag_buffer->year << "\n\r";
-        // check if have the track data
-		if (tag_buffer->comment[28] == '\0')
-		{
-            std::cout << "Comment: " << tag_buffer->comment << "\n\r";
-            std::cout << "Track: " << (int)tag_buffer->comment[29] << "\n\r";
-		}
-		else
-		{
-			std::cout << "Comment: " << tag_buffer->comment << "\n\r";
-		}
-        std::cout << "Genre: " << (int)tag_buffer->genre << "\n\r";
         return _LOAD_METADATA_SUCCESS_;
 	}
 	else
 	{
-		fprintf(stderr, "TAG Undefined\n");
+		fprintf(stderr, "Error: TAG Undefined\n");
 		return _LOAD_METADATE_FAIL_;
 	}
+}
+
+int printCollectedMetaData(Tag4Display tag_buffer)
+{
+    std::cout << "Title: " << tag_buffer.title << "\n\r";
+    std::cout << "Artist: " << tag_buffer.artist << "\n\r";
+    std::cout << "Album: " << tag_buffer.album << "\n\r";
+    std::cout << "Year: " << tag_buffer.year << "\n\r";
+    return 0;
 }
 
 }
